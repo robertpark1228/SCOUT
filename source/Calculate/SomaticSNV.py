04378e5a1bdf9f669c12b48e452261e3c696fbc1
Robert YC Park <robertpark1228@gmail.com>
2021-11-29 17:59:48 +0900

    Update SomaticSNV.py
    As python code doesn't work well, I've patched a few bugs and misspell

--- source/Calculate/SomaticSNV.py
+++ source/Calculate/SomaticSNV.py
@@ -5,9 +5,9 @@ import time
 
 HelpInfo = 'python SomaticSNV.py -i <single-cell.vcf> -g <Germline.vcf> -o <output.vcf>'
 parser=optparse.OptionParser(HelpInfo)
-parser.add_option('-i', '--input', dest='scSNV',type='string', help='single-cell vcf')
-parser.add_option('-g', '--germline', dest='Germline', type='string', help='Germline vcf')
-parser.add_option('-o', '--output', dest='outfile', type='string', help='output vcf')
+parser.add_option('-i', '--input', dest='scSNV',type=str, help='single-cell vcf')
+parser.add_option('-g', '--germline', dest='Germline', type=str, help='Germline vcf')
+parser.add_option('-o', '--output', dest='outfile', type=str, help='output vcf')
 options, args = parser.parse_args()
 def ParseVCF(aimvcf):
     # input a vcf file get the SNV positions and information into dict
@@ -22,21 +22,19 @@ def ParseVCF(aimvcf):
                 SNV_dict[Pos] = records
             else:
                 header += records
+
     return(SNV_dict, header)
 
-def main():
-    scdict, scHeader = ParseVCF(options.scSNV)
-    print('%s: Load %s SNVs from single-cell sample' % (time.ctimes(), len(scdict.keys())))
-    gmdict, gmHeader = ParseVCF(options.Germline)
-    print('%s: Load %s SNVs from germline sample' % (time.ctimes(), len(gmdict.keys())))
-    somatic_keys = scdict.keys() - gmdict.keys()
-    with open(options.outfile, 'w') as output:
-        output.write(scHeader)
-        output.flush()
-        for k in somatic_keys:
-            output.write(scdict[k])
-            output.flush()
-    print('%s: The somatic mutation has been written in file: %s' % (time.ctimes(), options.outfile))
 
-if "__name__" == "__main__":
-    main()
+scdict, scHeader = ParseVCF(options.scSNV)
+print('%s: Load %s SNVs from single-cell sample' % (time.ctime(), len(scdict.keys())))
+gmdict, gmHeader = ParseVCF(options.Germline)
+print('%s: Load %s SNVs from germline sample' % (time.ctime(), len(gmdict.keys())))
+somatic_keys = scdict.keys() - gmdict.keys()
+with open(options.outfile, 'w') as output:
+    output.write(scHeader)
+    output.flush()
+    for k in somatic_keys:
+        output.write(scdict[k])
+        output.flush()
+    print('%s: The somatic mutation has been written in file: %s' % (time.ctime(), options.outfile))
